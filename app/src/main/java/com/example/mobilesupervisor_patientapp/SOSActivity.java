a124c6c38f77d84cebb324efc2a453ced727a4ac
Kinga T <kinga-tanska@wp.pl>
2019-10-29 09:46:24 +0100

    Add Listener on pulse and steps

--- app/src/main/java/com/example/mobilesupervisor_patientapp/SOSActivity.java
+++ app/src/main/java/com/example/mobilesupervisor_patientapp/SOSActivity.java
@@ -4,10 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,30 +19,55 @@ import android.widget.EditText;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataSource;
 import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Value;
+import com.google.android.gms.fitness.request.DataSourcesRequest;
+import com.google.android.gms.fitness.request.OnDataPointListener;
+import com.google.android.gms.fitness.request.SensorRequest;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class SOSActivity extends FragmentActivity {
 
+    private static final String TAG = "SOSActivity";
     Button sendChat;
+    Button SmartbandResult;
+    GoogleSignInAccount account;
 
     public static final String EXTRA_MESSAGE = "ja";
     final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = System.identityHashCode(this) & 0xFFFF;
+    final int BODY_SENSORS_PERMISSIONS_REQUEST_CODE = System.identityHashCode(this) & 0xFFFF;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sos);
 
         sendChat = findViewById(R.id.sendChat);
+        SmartbandResult = findViewById(R.id.SmartbandResult);
 
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.BODY_SENSORS},
+                BODY_SENSORS_PERMISSIONS_REQUEST_CODE);
 
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        account = GoogleSignIn.getLastSignedInAccount(this);
         FitnessOptions fitnessOptions = FitnessOptions.builder()
                 .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
                 .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.TYPE_HEART_RATE_BPM, FitnessOptions.ACCESS_READ)
                 .build();
         if (!GoogleSignIn.hasPermissions(account, fitnessOptions)) {
             GoogleSignIn.requestPermissions(
@@ -66,7 +94,13 @@ public class SOSActivity extends FragmentActivity {
                 startActivity(a);
             }
         });
-
+        SmartbandResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(SOSActivity.this,SmartbandActivity.class);
+                startActivity(a);
+            }
+        });
 
         Intent intent = getIntent();
     }
@@ -75,6 +109,54 @@ public class SOSActivity extends FragmentActivity {
         //startActivity(intent);
     }
     private void accessGoogleFit() {
+        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
+                .addOnSuccessListener(new OnSuccessListener<DataSet>() {
+                    @Override
+                    public void onSuccess(DataSet dataSet) {
+                        Log.i(TAG, "Successfully subscribed");
+                        long total = dataSet.isEmpty() ? 0 : dataSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+                        Log.i(TAG, "Total steps: " + total);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i(TAG, "Problem and not subscribed");
+                    }
+                });
+        final OnDataPointListener heartRateListener = new OnDataPointListener() {
+            @Override
+            public void onDataPoint(DataPoint dataPoint) {
+                Log.i(TAG, "JESTEM TU");
+                for (Field field : dataPoint.getDataType().getFields()) {
+                    Value val = dataPoint.getValue(field);
+                    Log.i(TAG, "Detected DataPoint field: " + field.getName());
+                    Log.i(TAG, "Detected DataPoint value: " + val);
+                }
+            }
+        };
+        Fitness.getSensorsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .add(
+                        new SensorRequest.Builder()
+                        .setDataType(DataType.TYPE_HEART_RATE_BPM)
+                        .setSamplingRate(1, TimeUnit.MINUTES)
+                        .build(),
+                        heartRateListener
+                )
+                .addOnCompleteListener(
+                        new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.i(TAG, "LISTENER COMPLETED");
+                                }
+                                else {
+                                    Log.i(TAG, "LISTENER NOT COMPLETED", task.getException());
+                                }
+                            }
+                        }
+                );
     }
 
 }
---
+++ app/src/main/java/com/example/mobilesupervisor_patientapp/SmartbandActivity.java
@@ -0,0 +1,12 @@
+package com.example.mobilesupervisor_patientapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class SmartbandActivity extends FragmentActivity {
+    protected void OnCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smartband);
+    }
+}
