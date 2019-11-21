28e1c74b170b6a85dd67f3ed790894f98eab1d01
Tanska <kinga.tanska@intel.com>
2019-11-21 23:02:45 +0100

    Add read heart rate and steps. Send to firebase every result (must be changed in the future :))

--- app/src/main/java/com/example/mobilesupervisor_patientapp/SOSActivity.java
+++ app/src/main/java/com/example/mobilesupervisor_patientapp/SOSActivity.java
@@ -2,26 +2,39 @@ package com.example.mobilesupervisor_patientapp;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.ParcelUuid;
 import android.telephony.SmsManager;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -45,18 +58,46 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class SOSActivity extends FragmentActivity {
 
     private static final String TAG = "SOSActivity";
+    private static final int REQUEST_ENABLE_BT = 1;
+    public static long total;
     Button sendChat, SmartbandResult, sendPreparedMessage, videoChat;
     ImageButton sosBtn;
     GoogleSignInAccount account;
+    BluetoothManager btManager;
+    BluetoothAdapter btAdapter;
+    BluetoothDevice mBluetoothDevice;
+    public BluetoothGatt mBluetoothGatt;
+    private int mConnectionState = STATE_DISCONNECTED;
+    private static final int STATE_DISCONNECTED = 0;
+    private static final int STATE_CONNECTED = 2;
+    Button startScanningButton;
+    Button stopScanningButton;
+    Button connectButton;
+    Button disconnectButton;
+    public static List<ParcelUuid> MY_UUID;
+    UUID CLIENT_CHARACTERISTIC_CONFIG_UUID, HEART_RATE_SERVICE_UUID, HEART_RATE_MEASUREMENT_CHAR_UUID, HEART_RATE_CONTROL_POINT_CHAR_UUID;
 
-    //firebase auth
+    TextView peripheralTextView;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    public String mDeviceAddress;
+    public static Map<ParcelUuid, byte[]> mDeviceData;
+    private BluetoothLeScanner btScanner;
+    final UUID BATTERY_UUID = convertFromInteger(0xfee0);
+    final UUID BATTERY_LEVEL = convertFromInteger(0xff0c);
+
     FirebaseAuth firebaseAuth;
 
     //uid of the users
@@ -70,9 +111,16 @@ public class SOSActivity extends FragmentActivity {
     public static final String EXTRA_MESSAGE = "ja";
     final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = System.identityHashCode(this) & 0xFFFF;
     final int BODY_SENSORS_PERMISSIONS_REQUEST_CODE = System.identityHashCode(this) & 0xFFFF;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        HEART_RATE_SERVICE_UUID = convertFromInteger(0x180D);
+        HEART_RATE_MEASUREMENT_CHAR_UUID = convertFromInteger(0x2A37);
+        HEART_RATE_CONTROL_POINT_CHAR_UUID = convertFromInteger(0x2A39);
+        CLIENT_CHARACTERISTIC_CONFIG_UUID = convertFromInteger(0x2902);
+
         setContentView(R.layout.activity_sos);
 
         sendChat = findViewById(R.id.sendChat);
@@ -82,6 +130,27 @@ public class SOSActivity extends FragmentActivity {
         videoChat = findViewById(R.id.videoChat);
         sosBtn = findViewById(R.id.sosBtn);
 
+        btManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        btAdapter = btManager.getAdapter();
+        btScanner = btAdapter.getBluetoothLeScanner();
+        if (btAdapter == null || !btAdapter.isEnabled()) {
+            Intent enableBtIntent =
+                    new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+        if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("This app needs location access");
+            builder.setMessage("Please grant location access so this app can detect peripherals.");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                }
+            });
+            builder.show();
+        }
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.BODY_SENSORS},
                 BODY_SENSORS_PERMISSIONS_REQUEST_CODE);
@@ -148,8 +217,8 @@ public class SOSActivity extends FragmentActivity {
         SmartbandResult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent b = new Intent(SOSActivity.this,SmartbandActivity.class);
-                startActivity(b);
+                Intent a = new Intent(SOSActivity.this,SmartbandActivity.class);
+                startActivity(a);
             }
         });
 
@@ -169,11 +238,106 @@ public class SOSActivity extends FragmentActivity {
                 startActivity(intent);
             }
         });
-
-        Intent intent = getIntent();
-        
-
+        startScanning();
     }
+
+    public void startScanning() {
+        System.out.println("start scanning");
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                btScanner.startScan(leScanCallback);
+            }
+        });
+    }
+    public void stopScanning() {
+        System.out.println("stopping scanning");
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                btScanner.stopScan(leScanCallback);
+            }
+        });
+    }
+    public void connect() {
+
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        btAdapter = bluetoothManager.getAdapter();
+
+        final BluetoothDevice device = btAdapter
+                .getRemoteDevice(mDeviceAddress);
+        mBluetoothGatt = device.connectGatt(this, false, mGattCallback);
+    }
+    public UUID convertFromInteger(int i) {
+        final long MSB = 0x0000000000001000L;
+        final long LSB = 0x800000805f9b34fbL;
+        long value = i & 0xFFFFFFFF;
+        return new UUID(MSB | (value << 32), LSB);
+    }
+
+    public void disconnect() {
+        mBluetoothGatt.disconnect();
+    }
+
+
+    public ScanCallback leScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            processResult(result);
+        }
+
+        private void processResult(ScanResult result){
+            mBluetoothDevice = result.getDevice();
+            mDeviceAddress = result.getDevice().getAddress();
+            mDeviceData = result.getScanRecord().getServiceData();
+            Log.v("test", "ZNALZEZIONO" + mDeviceAddress);
+            if (mDeviceAddress.equals("F9:3C:B6:95:A4:1C")) {
+                Log.v("test", "ZNALZEZIONOooooooooooooooo" + mDeviceAddress);
+                MY_UUID = result.getScanRecord().getServiceUuids();
+                stopScanning();
+                connect();
+            }
+        }
+    };
+    BluetoothGattCallback mGattCallback  = new BluetoothGattCallback() {
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            if (newState == STATE_CONNECTED) {
+                gatt.discoverServices();
+            }
+        }
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            BluetoothGattCharacteristic characteristic =
+                    gatt.getService(HEART_RATE_SERVICE_UUID)
+                            .getCharacteristic(HEART_RATE_MEASUREMENT_CHAR_UUID);
+            gatt.setCharacteristicNotification(characteristic, true);
+            BluetoothGattDescriptor descriptor = characteristic.getDescriptor(CLIENT_CHARACTERISTIC_CONFIG_UUID);
+            descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+            gatt.writeDescriptor(descriptor);
+
+        }
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicRead(gatt, characteristic, status);
+            final Integer batteryLevel = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT8, 0);
+
+            if (batteryLevel != null) {
+                Log.d(TAG, "battery level: " + batteryLevel);
+            }
+        }
+        public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+
+            Log.e(TAG, "status"+status);
+            BluetoothGattCharacteristic characteristic = gatt.getService(HEART_RATE_SERVICE_UUID).getCharacteristic(HEART_RATE_CONTROL_POINT_CHAR_UUID);
+            characteristic.setValue(new byte[] {1,1});
+            gatt.writeCharacteristic(characteristic);
+        }
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            byte[] value = characteristic.getValue();
+            Log.e(TAG, "DATA: "+value[1]);
+            int HR = value[1];
+            sendHRtodb(HR);
+        }
+    };
 
     private void makePhoneCall() {
         if (ContextCompat.checkSelfPermission(SOSActivity.this,
@@ -196,11 +360,6 @@ public class SOSActivity extends FragmentActivity {
             }
         }
     }
-
-    public void ShowResults(View view){
-        //Intent intent = new Intent(this, DisplayMessageActivity.class);
-        //startActivity(intent);
-    }
     private void accessGoogleFit() {
         Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
                 .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
@@ -208,7 +367,8 @@ public class SOSActivity extends FragmentActivity {
                     @Override
                     public void onSuccess(DataSet dataSet) {
                         Log.i(TAG, "Successfully subscribed");
-                        long total = dataSet.isEmpty() ? 0 : dataSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+                        long res = dataSet.isEmpty() ? 0 : dataSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+                        total = res;
                         Log.i(TAG, "Total steps: " + total);
                     }
                 })
@@ -218,38 +378,6 @@ public class SOSActivity extends FragmentActivity {
                         Log.i(TAG, "Problem and not subscribed");
                     }
                 });
-        final OnDataPointListener heartRateListener = new OnDataPointListener() {
-            @Override
-            public void onDataPoint(DataPoint dataPoint) {
-                Log.i(TAG, "JESTEM TU");
-                for (Field field : dataPoint.getDataType().getFields()) {
-                    Value val = dataPoint.getValue(field);
-                    Log.i(TAG, "Detected DataPoint field: " + field.getName());
-                    Log.i(TAG, "Detected DataPoint value: " + val);
-                }
-            }
-        };
-        Fitness.getSensorsClient(this, GoogleSignIn.getLastSignedInAccount(this))
-                .add(
-                        new SensorRequest.Builder()
-                        .setDataType(DataType.TYPE_HEART_RATE_BPM)
-                        .setSamplingRate(1, TimeUnit.MINUTES)
-                        .build(),
-                        heartRateListener
-                )
-                .addOnCompleteListener(
-                        new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    Log.i(TAG, "LISTENER COMPLETED");
-                                }
-                                else {
-                                    Log.i(TAG, "LISTENER NOT COMPLETED", task.getException());
-                                }
-                            }
-                        }
-                );
     }
 
 
@@ -270,8 +398,16 @@ public class SOSActivity extends FragmentActivity {
         Toast.makeText(SOSActivity.this, "Wiadomość SOS została wysłana", Toast.LENGTH_SHORT).show();
 
     }
-
-
+    private void sendHRtodb(int HR) {
+        final DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Results");
+        Map<String, Object> PulseData = new HashMap<>();
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String strDate = dateFormat.format(date);
+        PulseData.put("Date", strDate);
+        PulseData.put("Heart Rate", HR);
+        reference.child("Heart Rate").child(strDate).setValue(PulseData);
+    }
 }
 
 
--- app/src/main/java/com/example/mobilesupervisor_patientapp/SmartbandActivity.java
+++ app/src/main/java/com/example/mobilesupervisor_patientapp/SmartbandActivity.java
@@ -1,12 +1,68 @@
 package com.example.mobilesupervisor_patientapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SmartbandActivity extends FragmentActivity {
-    protected void OnCreate(Bundle savedInstanceState) {
+    private static final String TAG = "SMARTBAND";
+    private long totalSteps;
+    FirebaseUser firebaseUser;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        totalSteps = SOSActivity.total;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smartband);
+        Log.i(TAG, "SMARTBAND STEPS: "+totalSteps);
+        TextView stepsCount = (TextView)findViewById(R.id.textView3);
+        stepsCount.setText(String.valueOf(totalSteps));
+        TextView info = (TextView)findViewById(R.id.textView2);
+        info.setText("Ilość kroków dziś: ");
+        TextView smart = (TextView)findViewById(R.id.textView5);
+        smart.setText("Wybierz okres podglądu wyników tętna: ");
+        sendStepstoDB(totalSteps);
+        makeStepsOutput(totalSteps, stepsCount);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Users");
+    }
+
+    private void makeStepsOutput(long Steps, TextView info) {
+        TextView output = (TextView)findViewById(R.id.textView4);
+        if (Steps < 2000) {
+            output.setText("Wybierz się na spacer :)");
+            info.setTextColor(this.getResources().getColor(R.color.Orange));
+        }
+        else {
+            output.setText("Świetnie!");
+            info.setTextColor(this.getResources().getColor(R.color.LightGreen));
+        }
+    }
+
+    private void sendStepstoDB(long steps) {
+        final DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Results");
+        Map<String, Object> Steps = new HashMap<>();
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String strDate = dateFormat.format(date);
+        Steps.put("Date", strDate);
+        Steps.put("Steps", steps);
+        reference.child("Steps").child(strDate).setValue(Steps);
     }
 }
